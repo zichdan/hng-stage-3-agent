0bfd7f60927e8b33dc0587b07f3fe393acf641b0
zichdan <zichdan1999@gmail.com>
2025-11-05 16:46:54 +0100

    add the command to nixpacks.toml start phase like so

---
+++ a2a_protocol/management/commands/createsu.py
@@ -0,0 +1,16 @@
+# a2a_protocol/management/commands/createsu.py
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                password='complexpassword123'
+            )
+        print('Superuser has been created.')
